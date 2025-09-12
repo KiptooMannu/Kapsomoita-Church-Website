5b4d90a461039cdf2a4abc6fb932cc0d4998489a
KiptooMannu <Kkgg7241@gmail.com>
2025-09-12 15:28:32 +0300

    all

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,26 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/Kapsomoita-Church-Website/">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import App from './App';
+// import './index.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/Kapsomoita-Church-Website/">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react() ,     tailwindcss()], 
+//    base: '/Kapsomoita-Church-Website/',
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,5 +14,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() ,     tailwindcss()], 
-   base: '/Kapsomoita-Church-Website/',
+   base: '/',
 })
